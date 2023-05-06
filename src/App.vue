4d3e9c5e4c8d9a99324f49363d8a2aab29d1ce7e
yamadicu <dicutia@gmail.com>
2023-05-06 10:18:16 +0200

    creazione progetto

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use './style/main.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+createApp(App).mount('#app')
